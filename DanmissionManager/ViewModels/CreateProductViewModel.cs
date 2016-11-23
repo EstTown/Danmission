1698b4084f431aae4a2b478e78fed04c9bafc99a
Dascham <sydowtwenty@gmail.com>
2016-11-23 15:52:35 +0100

    Added some view and viewmodels. changed startup view to "templateview".

---
+++ DanmissionManager/ViewModels/CreateProductViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DanmissionManager.ViewModels
+{
+    class CreateProductViewModel
+    {
+        public CreateProductViewModel()
+        {
+            product product = new product()
+            {
+                date = DateTime.Now,
+                desc = "Dett er et smart ur some kan..",
+                id = 05521,
+                name = "Rolex Ur",
+                price = 99.95
+            };
+            this.Product = product;
+        }
+
+        public product Product { get; set; }
+    }
+}
---
+++ DanmissionManager/ViewModels/TemplateViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Documents;
+
+namespace DanmissionManager.ViewModels
+{
+    class TemplateViewModel : BaseViewModel
+    {
+        public TemplateViewModel()
+        {
+            this._buttonText = "ThisCameFromTheViewModel";
+            this._input = String.Empty;
+
+
+            List<product> list = new List<product>();
+
+            product product = new product();
+        }
+
+        private string _buttonText;
+
+        public string ButtonText
+        {
+            get
+            {
+                return _buttonText;
+            }
+            set
+            {
+                _buttonText = value;
+                OnPropertyChanged("ButtonText");
+            }
+        }
+
+
+        private string _input;
+        public string Input
+        {
+            get
+            {
+                return _input;
+            }
+            set { _input = value; OnPropertyChanged("Input"); }
+        }
+        
+
+
+    }
+}
+
