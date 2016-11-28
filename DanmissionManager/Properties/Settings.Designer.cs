9a94b59d3289a70a23fb60680a0bfcb51f4956bd
EstTown <f_hansen@live.dk>
2016-11-28 16:15:17 +0100

    yeee

--- DanmissionManager/Properties/Settings.Designer.cs
+++ DanmissionManager/Properties/Settings.Designer.cs
@@ -130,5 +130,17 @@ namespace DanmissionManager.Properties {
                 this["IPPORT"] = value;
             }
         }
+        
+        [global::System.Configuration.UserScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("")]
+        public string GUICOLOR {
+            get {
+                return ((string)(this["GUICOLOR"]));
+            }
+            set {
+                this["GUICOLOR"] = value;
+            }
+        }
     }
 }
