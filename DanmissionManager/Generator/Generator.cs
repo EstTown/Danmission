d0ecbe1a9d5a8cbd92daf3becea9602fc3c60200
EstTown <f_hansen@live.dk>
2016-12-01 21:43:26 +0100

    Landsbytossen leger rundt med at sætte en generator sammen, udfra ObjectHydrator

---
+++ DanmissionManager/Generator/Generator.cs
@@ -0,0 +1,29 @@
+﻿using Foundation.ObjectHydrator;
+using Foundation.ObjectHydrator.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DanmissionManager
+{
+    class Generator
+    {
+        public Generator()
+        {
+            Hydrator<Product> _customerHydrator = new Hydrator<Product>().WithInteger(x => x.id, 1, 100)
+                .With(x => x.name, new ProductGenerator())
+                .WithInteger(x => x.category, 1, 50);
+            IList<Product> customerlist = _customerHydrator.GetList(20);
+
+            foreach (Product x in customerlist)
+            {
+                Console.WriteLine(x.id + "        " + x.name);
+            }
+        }
+
+
+
+    }
+}
--- DanmissionManager/ViewModels/MainViewModel.cs
+++ DanmissionManager/ViewModels/MainViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Foundation.ObjectHydrator;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,6 +15,10 @@ namespace DanmissionManager.ViewModels
         {
             ConsoleManager.Show();
             LoadSettings();
+
+            Generator a = new Generator();
+            
+
         }
 
         private void LoadSettings()
